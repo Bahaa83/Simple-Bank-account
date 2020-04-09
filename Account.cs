3989139ffe753284ccf907a8ca182525da4fad78
Bahaa83 <bahaa.love84@gmail.com>
2020-04-09 02:07:59 +0200

    Initial commit

---
+++ Account.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BANK
+{
+    public delegate void ChangeInTheAccount(object source,EventArgs e);
+    
+    class Account
+    {
+
+        public string Owner { get; set; }
+        public int Balance { get; set; }
+        public event ChangeInTheAccount WithdrwEvent;
+        public event ChangeInTheAccount DepositEvent;
+        public event ChangeInTheAccount ChangeOwnerEvent;
+        public event ChangeInTheAccount OverdraftAccountEvent;
+        public Account(string owner,int balance)
+        {
+            this.Owner = owner;
+            this.Balance = balance;
+        }
+        public Account()
+        {
+            
+        }
+        public void ChangeOwner(string newOwner)
+        {
+            if(this.Owner!=newOwner)
+            {
+                this.Owner = newOwner;
+                ChangeOwnerEvent?.Invoke(newOwner, new EventArgs());
+            }
+        }
+        public void Transaction(int amount)
+        {
+          
+            if (amount < 0)
+            {
+                if (this.Balance + amount >= 0)
+                {
+                   this.Balance =this.Balance += amount;
+                    WithdrwEvent?.Invoke(amount, new EventArgs());
+                }
+                else if(this.Balance + amount<0)
+                {
+                    OverdraftAccountEvent?.Invoke(this, new EventArgs());
+                }
+            }
+            if(amount > 0)
+            {
+                this.Balance = this.Balance + amount;
+                DepositEvent?.Invoke(amount, new EventArgs());
+            }
+        }
+      
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BANK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account newaccount = new Account("Bahaa", 3500);
+            newaccount.OverdraftAccountEvent += Newaccount_OverdraftAccountEvent;
+            newaccount.WithdrwEvent += Newaccount_WithdrwEvent;
+            newaccount.DepositEvent += Newaccount_DepositEvent;
+            newaccount.ChangeOwnerEvent += Newaccount_ChangeOwnerEvent;
+            newaccount.Transaction(3800);
+            Console.WriteLine("Kontoägaren : " + newaccount.Owner +"  Beloppet : " + newaccount.Balance);
+            newaccount.Transaction(-800);
+            Console.WriteLine("Kontoägaren : " + newaccount.Owner +"  Beloppet : " + newaccount.Balance);
+            newaccount.Transaction(300);
+            Console.WriteLine("Kontoägaren : " + newaccount.Owner +"  Beloppet : " + newaccount.Balance);
+            newaccount.Transaction(-4000);
+            Console.WriteLine("Kontoägaren : " + newaccount.Owner +"  Beloppet : " + newaccount.Balance);
+            newaccount.ChangeOwner("Jakob");
+            Console.WriteLine("Kontoägaren : " + newaccount.Owner +"  Beloppet : " + newaccount.Balance);
+
+            Console.ReadKey();
+           
+           
+        }
+
+        private static void Newaccount_ChangeOwnerEvent(object source, EventArgs e)
+        {
+            string newowner = (string)source;
+            Console.WriteLine("Kontoägaren hat bytt till ett nyt namn :  {0}  ", newowner);
+        }
+
+        static void Newaccount_DepositEvent(object source, EventArgs e)
+        {
+            int amount = Convert.ToInt32(source);
+            Console.WriteLine("{0} kr Har satts i ditt konto ",amount);
+           
+        }
+
+        static void Newaccount_WithdrwEvent(object source, EventArgs e)
+        {
+            int amount = Convert.ToInt32(source);
+            amount = amount * -1;
+            Console.WriteLine("{0} kr har dragit av ditt konto", amount);
+        }
+
+        static void Newaccount_OverdraftAccountEvent(object source, EventArgs e)
+        {
+            int  amount = Convert.ToInt32(source);
+            amount = amount * -1;
+            Console.WriteLine("Överföringen misslyckades !\n Beloppet i ditt konto tillräckligt inte att överföra {0} kr", amount);
+        }
+    }
+}
